f1f1a4846660ba44198ff7e2da21c90500b93886
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2025-12-03 11:00:58 +0700

    fix: Error: ENOENT: no such file or directory, stat /opt/render/project/src/BE/FE/dist/index.html

--- BE/server.js
+++ BE/server.js
@@ -18,9 +18,10 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '/FE/dist')));
+const root = path.resolve();
+app.use(express.static(path.join(root, '/FE/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/FE/dist/index.html'));
+  res.sendFile(path.join(root, '/FE/dist/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
